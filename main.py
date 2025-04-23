a42f474ccfc12d4d3d9fdf5a49bd62a14a8b3b89
Jan Lo <loujiayu7@gmail.com>
2025-04-23 14:07:33 +0800

    change port

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+	app.run(host="0.0.0.0", port=int(os.environ.get("WEBSITES_PORT", 8080)))
